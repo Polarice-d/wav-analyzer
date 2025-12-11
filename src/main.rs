e4286c95f434b4016a472b2bab247a1cae2129c4
Polarice-d <thenextcomment2@gmail.com>
2025-12-10 21:28:23 -0600

    Added basic parsing functionality with CLAP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::path::PathBuf;
+
 use hound;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about= None)]
+struct Args {
+    /// File path of the WAV file
+    #[arg(short, long, value_name = "FILE")]
+    path: PathBuf,
+}
 
 fn main() {
-    let mut reader = hound::WavReader::open("samples/41khz_16bit.wav").unwrap();
+    let args = Args::parse();
+    let mut reader = hound::WavReader::open(args.path.as_path()).unwrap();
     let num = reader.samples::<i16>().count(); // I think this is the number of samples but i'm not sure
     println!("{num}");
 }
